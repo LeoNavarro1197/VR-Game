ffb5c9fbbe35b9eefc45a8303c9879a9668fa9a1
Yeison Navarro <53413486+LeoNavarro1197@users.noreply.github.com>
2024-07-11 17:40:27 -0500

    Finish Formula

--- Assets/Proyecto/Scripts/ActiveEquation.cs
+++ Assets/Proyecto/Scripts/ActiveEquation.cs
@@ -16,13 +16,6 @@ public class ActiveEquation : MonoBehaviour
 
     public GameObject dragAndDrop, alphaNumerico;
 
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
--- Assets/Proyecto/Scripts/AlphaNumerico.cs
+++ Assets/Proyecto/Scripts/AlphaNumerico.cs
@@ -5,9 +5,13 @@ using TMPro;
 
 public class AlphaNumerico : MonoBehaviour
 {
-    public TextMeshProUGUI existencias, dias, agosto, octubre, noviembre;
+    public TextMeshProUGUI existencias, dias, dias1, agosto, octubre, noviembre;
     public TextMeshProUGUI agostoE, octubreE, noviembreE;
-    public TextMeshProUGUI resultadoCPM;
+    public TextMeshProUGUI resultadoCPM, resultadoCPMDividido, resultadoCPMDividido1, resultadoCPMDividido2, resultadoCPMDividido3;
+    public TextMeshProUGUI fraccionTiempoResultado, fraccionTiempoResultadoAprox, fraccionTiempoResultadoAprox1, fraccionTiempoResultadoAprox2;
+    public TextMeshProUGUI PRFrascos, NMEResultado;
+
+    public string CAP;
 
     // Start is called before the first frame update
     void Start()
@@ -22,13 +26,31 @@ public class AlphaNumerico : MonoBehaviour
         octubreE.text = octubre.text;
         noviembreE.text = noviembre.text;
 
-        
+        int intAgostoE = int.Parse(agostoE.text);
+        int intOctubreE = int.Parse(octubreE.text);
+        int intNoviembreE = int.Parse(noviembreE.text);
 
-    }
+        resultadoCPM.text = (intAgostoE + intOctubreE + intNoviembreE).ToString();
+        resultadoCPMDividido.text = (int.Parse(resultadoCPM.text) / 3).ToString();
+        resultadoCPMDividido1.text = resultadoCPMDividido.text;
+        resultadoCPMDividido2.text = resultadoCPMDividido1.text;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        dias.text = dias1.text;
+
+        fraccionTiempoResultado.text = (int.Parse(resultadoCPMDividido.text) * 0.26f).ToString();
+        fraccionTiempoResultadoAprox.text = fraccionTiempoResultado.text;
+        float i = float.Parse(fraccionTiempoResultadoAprox.text);
+        i = Mathf.FloorToInt(i);
+        fraccionTiempoResultadoAprox.text = i.ToString();
+        fraccionTiempoResultadoAprox1.text = fraccionTiempoResultadoAprox.text;
+
+        PRFrascos.text = (i * 2).ToString();
+
+        fraccionTiempoResultadoAprox2.text = fraccionTiempoResultadoAprox1.text;
+        resultadoCPMDividido3.text = resultadoCPMDividido2.text;
+
+        NMEResultado.text = ((int.Parse(fraccionTiempoResultadoAprox2.text)) + (int.Parse(resultadoCPMDividido3.text))).ToString();
+
+        CAP = (int.Parse(PRFrascos.text) + (int.Parse(resultadoCPMDividido.text) * 1) - int.Parse(existencias.text)).ToString();
     }
 }
