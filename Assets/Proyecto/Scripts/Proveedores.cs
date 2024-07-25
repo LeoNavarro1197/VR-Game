01388ce2acfa2f0ddd3afbecd3199ee238fcb717
Yeison Navarro <53413486+LeoNavarro1197@users.noreply.github.com>
2024-07-25 16:35:55 -0500

    Normal Update Acta

--- Assets/Proyecto/Scripts/Proveedores.cs
+++ Assets/Proyecto/Scripts/Proveedores.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class Proveedores : MonoBehaviour
 {
-    public GameObject[] buttonPrefabs; // Array de prefabs de los botones
-    public RectTransform canvasRectTransform; // RectTransform del Canvas
-    public Vector3[] positions; // Posiciones preestablecidas para los botones
+    public GameObject[] buttonPrefabs;
+    public RectTransform canvasRectTransform;
+    public Vector3[] positions;
 
     void Start()
     {
@@ -27,28 +27,17 @@ public class Proveedores : MonoBehaviour
 
     void PlaceButtons()
     {
-        // Crear una lista de índices de posiciones disponibles
         List<int> availableIndices = new List<int> { 0, 1, 2 };
 
         for (int i = 0; i < buttonPrefabs.Length; i++)
         {
-            // Obtener un índice aleatorio de la lista de índices disponibles
             int randomIndex = Random.Range(0, availableIndices.Count);
-
-            // Obtener la posición correspondiente al índice aleatorio
             Vector2 position = positions[availableIndices[randomIndex]];
 
-
-            // Eliminar el índice de la lista para evitar repetición
             availableIndices.RemoveAt(randomIndex);
-
-            // Instanciar el botón correspondiente al prefab
             GameObject newButton = Instantiate(buttonPrefabs[i], canvasRectTransform);
 
-            // Obtener el RectTransform del nuevo botón
             RectTransform buttonRectTransform = newButton.GetComponent<RectTransform>();
-
-            // Establecer la posición del botón
             buttonRectTransform.anchoredPosition = position;
         }
     }
